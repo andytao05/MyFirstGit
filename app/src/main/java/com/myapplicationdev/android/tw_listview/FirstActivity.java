e4c8f6634ed70dcf6f18e11ba9696f5e27a57f9f
andytao05 <andytaodev@gmail.com>
2019-01-24 11:54:28 +0800

    Modify activity_main and FirstActivity

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,9 +25,23 @@ public class FirstActivity extends AppCompatActivity {
         al = new ArrayList<String>();
         al.add("Year 1");
 
+        //***Start***
+        al.add("Year 2");
+        al.add("Year 3");
 
+        aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, al);
 
+        lv = findViewById(R.id.years);
+        lv.setAdapter(aa);
 
-
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getBaseContext(), SecondActivity.class);
+                intent.putExtra("pos", position);
+                startActivity(intent);
+            }
+        });
+        //***End***
     }
 }
